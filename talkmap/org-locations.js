bdbf1a06611c38067298fbe781a0615439aa5198
pietropic <69018641+pietropic@users.noreply.github.com>
2025-11-21 15:31:51 +0100

    talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,92 @@
 var addressPoints = [
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "S\u00e9minaire des Th\u00e9sards of the IMJ<br />IMJ-PRG, Universit\u00e9 Paris Cit\u00e9; Paris,  France",
+    48.8588897,
+    2.320041
   ],
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.5074456,
-    -0.1277653
+    "S\u00e9minaire syst\u00e8mes dynamiques et g\u00e9om\u00e9trie<br />Laboratoire Angevin de Recherche en Math\u00e9matiques, Universit\u00e9 d'Angers; Angers, France",
+    47.4739884,
+    -0.5515588
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
+    "Any Complex Geometry Seminar<br />University of California Berkeley; Berkeley, USA",
     37.8708393,
     -122.272863
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Complex Analysis Seminar<br />University of Bayreuth; Bayreuth, Germany",
+    49.9446345,
+    11.5743543
+  ],
+  [
+    "KASS Seminar<br />Chalmers University; Gothenburg, Sweden",
+    57.7072326,
+    11.9670171
+  ],
+  [
+    "Geometry Webinar AmSur/AmSul<br />Webinar AmSur/AmSul; Online",
+    43.5904719,
+    3.8595132
+  ],
+  [
+    "Graduate Student Seminar Series<br />SLMath; Berkeley, USA",
+    37.8708393,
+    -122.272863
+  ],
+  [
+    "MARGE in Brest: Fibrations and Deformations<br />Universit\u00e9 de Bretagne Occidentale; Brest, France",
+    48.3905283,
+    -4.4860088
+  ],
+  [
+    "Complex geometry seminar<br />Mathematisches Institut, University of M\u00fcnster; M\u00fcnster, Germany",
+    51.9625101,
+    7.6251879
+  ],
+  [
+    "Mathematics Seminar<br />Department of Mathematics, Aarhus University.; Aarhus, Denmark",
+    56.1496278,
+    10.2134046
+  ],
+  [
+    "Geometry seminar at MPI Leipzig<br />Max Plank Institute for Mathematics in the Sciences; Leipzig, Germany",
+    51.3406321,
+    12.3747329
+  ],
+  [
+    "Geometric Analysis Seminar<br />BICMR; Online",
+    43.5904719,
+    3.8595132
+  ],
+  [
+    "Workshop on Singular canonical K\u00e4hler metrics on compact and non-compact manifolds<br />R\u00e9nyi Institute; Budapest, Hungary",
+    47.4978789,
+    19.0402383
+  ],
+  [
+    "Demi-Journ\u00e9e des doctorant.e.s de l\u2019\u00e9quipe TGA 2024<br />IMJ-PRG, Sorbonne Universit\u00e9; Paris, France",
+    48.8588897,
+    2.320041
+  ],
+  [
+    "S\u00e9minaire de g\u00e9om\u00e9trie alg\u00e9brique de Jussieu<br />ENS, salle W; Paris, France",
+    48.8588897,
+    2.320041
+  ],
+  [
+    "Algebraic Geometry Seminar<br />University of Glasgow; Glasgow, Scotland",
+    55.861155,
+    -4.2501687
+  ],
+  [
+    "S\u00e9minaire G\u00e9om\u00e9tries ICJ<br />Institut Camille Jordan, Universit\u00e9 Claude Bernard Lyon 1; Lyon, France",
+    45.7578137,
+    4.8320114
+  ],
+  [
+    "S\u00e9minaire de G\u00e9om\u00e9trie complexe<br />Institut de Math\u00e9matiques de Toulouse, Universit\u00e9 de Toulouse; Toulouse, France",
+    43.6044638,
+    1.4442433
   ]
 ];
